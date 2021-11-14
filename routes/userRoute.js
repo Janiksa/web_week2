9fdf88de4999a5d9071c1202c5b1420113de2668
Jani Salo <janikristian.salo@gmail.com>
2021-11-14 15:08:19 +0200

    week 3

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -12,7 +12,7 @@ const multer = require('multer');
 router.route('/')
     .get(user_list_get)
     .post(
-        body("name").isLength({min: 8}),
+        body("name").isLength({min: 3}),
         body("email").isEmail(),
         body("passwd").matches("(?=.*[A-Z]).{8,}"),
         user_post);
